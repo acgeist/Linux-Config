b24cdeb50104597169295c11e4de25abd16ce192
Andrew Geist <acgeist@gmail.com>
2017-04-01 14:39:56 -0400

    automated commit 20170401

--- acg_config.sh
+++ acg_config.sh
@@ -4,3 +4,4 @@ cp .bashrc ~/.bashrc
 cp .tmux.conf ~/.tmux.conf
 cp acg_color.vim /usr/share/vim/vim74/colors/acg_color.vim
 cp .bp.c $HOME/.vim/.bp.c
+cp .bp.sh $HOME/.vim/.bp.sh
--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ cp ~/.bashrc .bashrc
 cp ~/.tmux.conf .tmux.conf
 cp /usr/share/vim/vim74/colors/acg_color.vim acg_color.vim
 cp $HOME/.vim/.bp.c .bp.c
+cp $HOME/.vim/.bp.sh .bp.sh
 git add .
 git commit -m "automated commit $(date +%Y%m%d)"
 git push origin master
